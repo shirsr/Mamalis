947cf693a95183b0c4cb1e5e5b53b6b6ba9c9f82
shir sharf <shirsharff@gmail.com>
2021-06-26 20:20:49 +0300

    Employee abstractclass- without Degree

---
+++ Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+/*
+ * This is an abstract class of employee
+ * here we define the general features of employee
+ */
+abstract class Employee
+{
+	protected string _employeeName;
+	protected string _employeeID;
+	protected string _jobTitle;
+	//Degree 
+	protected float _hoursOfWork;
+	protected float _salary;
+
+
+	public virtual float GetSalary() { return this._salary; } //abstract method
+}
