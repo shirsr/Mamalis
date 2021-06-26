23b84005dafbd323fe442e7c22bd50b9486e0947
shirsr <55427198+shirsr@users.noreply.github.com>
2021-06-26 19:38:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MamaLis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
