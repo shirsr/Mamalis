f24bbc72679b6ff3745780f86af7001d27d9b345
shir sharf <shirsharff@gmail.com>
2021-06-26 19:43:02 +0300

    test of commit from VS

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MamaLis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
+            Console.WriteLine("Welcome to MamaLis Project !");
         }
     }
 }
