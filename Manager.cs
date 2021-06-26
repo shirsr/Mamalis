a17bacbad877f85340c0a9a6f49b43fbf6f09143
shir sharf <shirsharff@gmail.com>
2021-06-26 20:37:46 +0300

    adding tree inheritance classes from Employee- senior, junior, manager.

---
+++ Manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaLis
+{
+    class Manager : Employee
+    {
+        public override float GetSalary() { return this._salary; }
+    }
+}
---
+++ SeniorEmployee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaLis
+{
+    class SeniorEmployee : Employee
+    {
+     public override float GetSalary() { return this._salary; }
+    }
+}
